a801fa267a43599d172a90415852fbf7e9f8a1cb
IcyKit <icykitdesign@gmail.com>
2026-01-16 21:56:57 +0300

    grafana added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrometheusController } from './prometheus/prometheus.controller';
+import { PrometheusService } from './prometheus/prometheus.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, PrometheusController],
+  providers: [AppService, PrometheusService],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getError(er: number) {
+    if (er == 200) {
+      return {
+        data: 'Hello',
+      };
+    }
+    throw new HttpException('Ошибочка!', er);
   }
 }
